36e9efd2e854d07d9009c48b0138c868be7d18c4
17IlyaVasilev <73096983+17IlyaVasilev@users.noreply.github.com>
2020-10-24 18:42:13 +0300

    Update 2 Контест C.cpp

--- 2 Контест C.cpp
+++ 2 Контест C.cpp
@@ -1,8 +1,22 @@
-﻿#include <iostream>
+#include <iostream>
 #include <algorithm>
 #include <vector>
 
 using namespace std;
+
+void Partition(int* a, int& l, int& r, int k) {
+	int i = l + 1, j = r, cur = a[i];
+	while (true) {
+		while (a[++i] < cur);
+		while (a[--j] > cur);
+		if (i > j) break;
+		swap(a[i], a[j]);
+	}
+	a[l + 1] = a[j];
+	a[j] = cur;
+	if (j >= k) r = j - 1;
+	if (j <= k) l = i;
+}
 
 int KTH(int* a, int n, int k)
 {
@@ -18,19 +32,11 @@ int KTH(int* a, int n, int k)
 		if (a[l] > a[r]) swap(a[l], a[r]);
 		if (a[l + 1] > a[r]) swap(a[l + 1], a[r]);
 		if (a[l] > a[l + 1]) swap(a[l], a[l + 1]);
-		int i = l + 1, j = r, cur = a[i];
-		while (true) {
-			while (a[++i] < cur);
-			while (a[--j] > cur);
-			if (i > j) break;
-			swap(a[i], a[j]);
-		}
-		a[l + 1] = a[j];
-		a[j] = cur;
-		if (j >= k) r = j - 1;
-		if (j <= k) l = i;
+
+		Partition(a, l, r, k);
 	}
 }
+
 
 int main() {
 	ios_base::sync_with_stdio(false);
